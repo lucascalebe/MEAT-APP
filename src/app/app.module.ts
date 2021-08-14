8e8e1fcaf020d7416132d828d265cfa4897ea3ef
Lucas Calebe <lcaguiar@stefanini.com>
2021-08-14 00:26:12 -0300

    76. Carregamento Tardio de Módulos (lazy-loading)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,36 @@
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { RestaurantService } from './restaurants/restaurants.service';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { ROUTES } from './app.routes';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
+import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderComponent } from './order/order.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OrderService } from './order/order.service';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantService } from './restaurants/restaurants.service';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { RatingComponent } from './shared/rating/rating.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ import { Routes } from "@angular/router";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
             {path: '', redirectTo: 'menu', pathMatch: 'full'},
