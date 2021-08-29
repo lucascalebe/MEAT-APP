5192b7cfe0f6f27ffa073a51e1df01816452dcfc
Lucas Calebe <lcaguiar@stefanini.com>
2021-08-29 14:44:27 -0300

    87. Usando os operadores do e switchMap para corrigir subscribes de time concorrendo entre si

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,5 +1,7 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/switchMap'
 import { NotificationService } from './../notification.service';
 import { Component, OnInit } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
@@ -32,11 +34,12 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message => {
+    this.notificationService.notifier
+    .do(message => {
       this.message = message;
       this.snackVisibility = 'visible';
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden')
-    });
+    }).switchMap(message => Observable.timer(3000))
+    .subscribe(timer => this.snackVisibility = 'hidden')
   }
 
 }
