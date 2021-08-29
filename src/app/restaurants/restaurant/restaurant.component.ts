a3d126f6d3b5997883adff45098818cf13017bcb
Lucas Calebe <lcaguiar@stefanini.com>
2021-08-29 14:53:59 -0300

    88. Animação com void e estilos em transições (Animação de entrada em Restaurantes)

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,11 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant.model';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantAppeared', [
+      state('ready', style({opcacity: 1})),
+      transition('void => ready',[
+        style({opacity: 0, transform: 'translate(-30px, -10px}'}),
+        animate('400ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready'
 
   @Input() restaurant: Restaurant;
 
